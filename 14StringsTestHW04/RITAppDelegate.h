88e0061bea06656f84490e43a74aab7a9f5c31e4
Alexander Pronin <pronin.alx@gmail.com>
2014-02-14 01:33:07 +0200

    Initial Commit

---
+++ 14StringsTestHW04/RITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RITAppDelegate.h
+//  14StringsTestHW04
+//
+//  Created by Pronin Alexander on 14.02.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 14StringsTestHW04Tests/_4StringsTestHW04Tests.m
@@ -0,0 +1,34 @@
+//
+//  _4StringsTestHW04Tests.m
+//  14StringsTestHW04Tests
+//
+//  Created by Pronin Alexander on 14.02.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _4StringsTestHW04Tests : XCTestCase
+
+@end
+
+@implementation _4StringsTestHW04Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
